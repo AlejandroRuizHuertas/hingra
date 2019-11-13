c6baf8fc7c8fe451880cf1b31fe4709445ce2f3d
AlejandroRuizHuertas <peditito@gmail.com>
2019-11-13 20:46:30 +0100

    Añadida iteración 3

---
+++ Iteracion3/src/Dominio/Gestor_Box.java
@@ -0,0 +1,23 @@
+package Dominio;
+
+public class Gestor_Box {
+
+	/**
+	 * 
+	 * @param xox
+	 */
+	public void reservar_Box(int xox) {
+		// TODO - implement Gestor_Box.reservar_Box
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param box
+	 */
+	public void ver_turnos(int box) {
+		// TODO - implement Gestor_Box.ver_turnos
+		throw new UnsupportedOperationException();
+	}
+
+}
