11acb46e831833ff4fe1a5eade1255476ba6ae4f
AlejandroRuizHuertas <peditito@gmail.com>
2019-11-20 19:50:51 +0100

    Iteración 6 hecha

---
+++ Iteracion6/src/Dominio2/Box.java
@@ -0,0 +1,11 @@
+package Dominio2;
+
+public class Box {
+
+	private string estado;
+	private int id;
+	private int turnos;
+	private string usuario;
+	private string mecánico;
+
+}
---
+++ Iteracion6/src/Seleccionar_box_(Diseño)/Box.java
@@ -0,0 +1,10 @@
+package Seleccionar_box_(Diseño);
+
+public class Box {
+
+	private String estado;
+	private int id = 0;
+	private int turnos;
+	private String mecanico;
+
+}
