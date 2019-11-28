a44a7b414600e5e72f9f0cdcd955551cb55eccd1
Alejandro <peditito@gmail.com>
2019-11-28 19:07:35 +0100

    Añadido Ver piezas compatibles y corregidos archivos del pom

---
+++ Iteracion9/src/Coche.java
@@ -0,0 +1,18 @@
+public class Coche {
+
+	private ArrayList<Pieza> piezas;
+
+	public ArrayList<Pieza> getPiezas() {
+		return this.piezas;
+	}
+
+	/**
+	 * 
+	 * @param piezas
+	 */
+	public void setPiezas(int piezas) {
+		// TODO - implement Coche.setPiezas
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Iteracion9/src/Dominio/Gestor_Almacen.java
@@ -0,0 +1,23 @@
+package Dominio;
+
+public class Gestor_Almacen {
+
+	/**
+	 * 
+	 * @param piezas
+	 */
+	public ArrayList[] Obtener_piezas(int piezas) {
+		// TODO - implement Gestor_Almacen.Obtener_piezas
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param piezas
+	 */
+	public void Set_stock(int piezas) {
+		// TODO - implement Gestor_Almacen.Set_stock
+		throw new UnsupportedOperationException();
+	}
+
+}
